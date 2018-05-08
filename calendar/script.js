3c4caa1c02ff54649276a20d1b9400fb3e0deebf
Jin Hui Chen <jinhuichen@172-30-21-111.DYNAPOOL.NYU.EDU>
2018-05-07 20:41:20 -0400

    Add calendar functionality

---
+++ calendar/script.js
@@ -0,0 +1,32 @@
+
+function onSignIn(googleUser) {
+
+  var profile = googleUser.getBasicProfile();
+  $(".g-signin2").css("display", "none" );
+
+  $("#pic").attr("src", profile.getImageUrl());
+
+  $("#email").text(profile.getEmail());
+
+  $("#username").text(profile.getName());
+
+  $("#calendar").css("display", "block");
+
+  $("#profile").text("Your Profile");
+
+  $("#set_calendar").css("display", "block");
+
+
+}
+  function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+
+    $("#invisible").css("display", "none");
+    $(".g-signin2").css("display", "block" );
+    $(".data").css("display", "none");
+    setTimeout(function(){ window.location = 'http://localhost:8888/Group3/'; }, 1500);
+    });
+
+
+  }
